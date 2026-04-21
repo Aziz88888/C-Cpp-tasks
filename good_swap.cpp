8ab66f07f9dd72f5863a84507b06bb4fd00881af
Aziz <108368503+Aziz88888@users.noreply.github.com>
2026-04-21 14:17:04 +0300

    Add files via upload
    correct swap file

---
+++ good_swap.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+void swap(int* a, int* b);
+
+int main() {
+
+	int c = 3;
+	int d = 5;
+	std::cout << "Before swap.\n";
+	std::cout << "c = " << c << ", " << "d = " << d << std::endl;
+	swap(&c, &d);
+	std::cout << "After swap.\n";
+	std::cout << "c = " << c << ", " << "d = " << d << std::endl;
+
+	return 0;
+}
+
+
+void swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
