356ccb5f3310cf8f97a20761f42e13228b3dea8f
Aziz <108368503+Aziz88888@users.noreply.github.com>
2026-05-14 15:33:03 +0300

    how to reverse a string
    string_reverse

---
+++ string_reverse.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+void string_reverse(char* str){
+	int start = 0;
+	int end = strlen(str) - 1;
+	char tmp;
+	
+	while(start < end){
+		tmp = str[start];
+		str[start] = str[end];
+		str[end] = tmp;
+		start++;
+		end--;
+	}
+}
+
+int main(){
+	char my_str[] = "Hello, World!";
+	string_reverse(my_str);
+	printf("%s\n", my_str);
+	return 0;
+}
+
+
+
